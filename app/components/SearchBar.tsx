08fc493cab75ba9776d87b95020d2f58c30e974b
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2023-08-30 22:53:36 -0300

    new: custom search bar component

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,33 @@
+import { SearchBar } from "@rneui/themed";
+import { useState } from "react";
+
+export default function CustomSearchBar() {
+    const [search, setSearch] = useState("");
+
+    const updateSearch = (search: any) => {
+        setSearch(search);
+    };
+
+    return (
+        <SearchBar
+            ref={(search) => (this.search = search)}
+            style={{
+                padding: 8,
+            }}
+            placeholder="Pesquisar formulários..."
+            onChangeText={updateSearch}
+            value={search}
+            lightTheme={true}
+            inputStyle={{
+                backgroundColor: "white",
+            }}
+            inputContainerStyle={{
+                backgroundColor: "white",
+            }}
+            containerStyle={{
+                backgroundColor: "#f1f1f1",
+                borderRadius: 8,
+            }}
+        />
+    );
+}
