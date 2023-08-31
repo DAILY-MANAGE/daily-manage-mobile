dce11675a9b7e4b76b0ec3b135917fcfbdc7c072
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2023-08-30 22:50:52 -0300

    fix: refactor code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,23 @@
-import { Stack } from 'expo-router'
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function AppLayout() {
     return (
         <SafeAreaProvider>
             <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false, headerLargeTitle: true }} />
-                <Stack.Screen name="criarFormulario" options={{ presentation: 'modal' }} />
-                <Stack.Screen name="form" options={{ presentation: "formSheet" }} />
+                <Stack.Screen
+                    name="login"
+                    options={{ presentation: "formSheet" }}
+                />
+                <Stack.Screen
+                    name="(tabs)"
+                    options={{ headerShown: false, headerLargeTitle: true }}
+                />
+                <Stack.Screen
+                    name="createForm"
+                    options={{ presentation: "modal" }}
+                />
+                <Stack.Screen name="form" options={{ presentation: "modal" }} />
             </Stack>
         </SafeAreaProvider>
     );
