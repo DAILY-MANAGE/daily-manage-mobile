fbe5d23054ed81d46a2e55cba0b08017e87cc214
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2023-08-30 22:55:18 -0300

    new: login fields

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,10 +1,21 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { Input } from "@rneui/base";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 export default function Login() {
     return (
-        <Link href="/home">
-            <Text>Oi Login</Text>
-        </Link>
+        <SafeAreaView>
+            <StatusBar />
+            <View>
+                <Text>Usuário</Text>
+                <Input placeholder="BASIC INPUT" />
+            </View>
+            <View>
+                <Text>Senha</Text>
+                <Input placeholder="BASIC INPUT" secureTextEntry={true} />
+            </View>
+        </SafeAreaView>
     );
 }
