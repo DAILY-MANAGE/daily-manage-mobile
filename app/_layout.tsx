dd55a2198ab927de4006d3edbb0d37f000011164
arthurm9 <arthurmarquesoliveira001@gmail.com>
2023-09-04 12:18:54 -0300

    new: install nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,16 +5,10 @@ export default function AppLayout() {
   return (
     <SafeAreaProvider>
       <Stack>
-        {/*  <Stack.Screen
-                    name="login"
-                    options={{ presentation: "formSheet" }}
-                /> */}
         <Stack.Screen
           name="(tabs)"
           options={{ headerShown: false, headerLargeTitle: true }}
         />
-        {/*         <Stack.Screen name="createForm" options={{ presentation: "modal" }} /> */}
-{/*         <Stack.Screen name="form" options={{ presentation: "modal" }} /> */} 
       </Stack>
     </SafeAreaProvider>
   );
