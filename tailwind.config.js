4db11772b4efcde7587d8ecea3f386bac24cd049
Arthur Marques de Oliveira <arthurmarquesoliveira001@gmail.com>
2023-09-11 20:22:54 -0300

    add: tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-
+    "../src/app/_layout.tsx",
+    "../src/app/**/_layout.tsx",
+    "../src/app/**/*.tsx",
+    "../src/app/**/**/_layout.tsx",
+    "../src/app/**/**/*.tsx",
   ],
   theme: {
     extend: {},
