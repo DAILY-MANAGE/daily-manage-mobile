76a7e7f5451e7382d8f729792467ee64975e40d4
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-10-14 13:03:37 -0300

    changes: moved

---
+++ src/app/components/Button/index.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@rneui/base";
+
+interface ButtonProps {
+  onPress: string | any;
+  title: string;
+  color?: string;
+  type?: "solid" | "outline";
+  radius?: "md" | "sm" | "lg";
+  size?: "md" | "sm" | "lg";
+  buttonStyle?: any;
+  titleStyle?: any;
+}
+
+ButtonComponent.defaultProps = {
+  radius: "md",
+  size: "lg",
+  color: "black",
+  type: "solid",
+  titleStyle: "",
+  buttonStyle: "",
+};
+
+export default function ButtonComponent({
+  onPress,
+  radius,
+  size,
+  title,
+  color,
+  type,
+  buttonStyle,
+  titleStyle,
+}: ButtonProps) {
+  return (
+    <Button
+      onPressIn={onPress}
+      radius={radius}
+      color={color}
+      size={size}
+      title={title}
+      type={type}
+      buttonStyle={buttonStyle}
+      titleStyle={titleStyle}
+    />
+  );
+}
