54d4fec283fdc85aaee1857748c4cbc68b62ba83
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-10-15 22:30:52 -0300

    new: team overlay component

---
+++ src/app/equipe/components/Overlay.tsx
@@ -0,0 +1,62 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { Overlay } from "@rneui/themed";
+import { View, Text, StyleSheet } from "react-native";
+import ButtonComponent from "../../components/Button";
+import InputComponent from "../../(auth)/components/Input";
+
+interface OverlayComponentProps {
+  value: string | null;
+  setValue: React.Dispatch<React.SetStateAction<string | null>>;
+  onPress: () => any;
+  editable: boolean;
+}
+
+export default function OverlayComponent({
+  value,
+  setValue,
+  onPress,
+  editable,
+}: OverlayComponentProps) {
+  const [visible, setVisible] = useState(false);
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <View>
+      <ButtonComponent onPress={toggleOverlay} title='Nova Equipe +' />
+      <Overlay
+        overlayStyle={styles.overlayStyle}
+        isVisible={visible}
+        onBackdropPress={toggleOverlay}
+      >
+        <Text style={styles.textPrimary}>Nova Equipe</Text>
+        <InputComponent
+          placeholder='Digite o nome da Equipe'
+          value={value}
+          setValue={setValue}
+          label='Nome da Equipe'
+          editable={editable}
+        />
+        <ButtonComponent onPress={onPress} title='Salvar' />
+      </Overlay>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  overlayStyle: {
+    borderRadius: 16,
+    padding: 16,
+    width: "90%",
+    height: "auto",
+    margin: 0,
+    gap: 16,
+  },
+  textPrimary: {
+    paddingTop: 8,
+    textAlign: "center",
+    fontSize: 20,
+  },
+});
