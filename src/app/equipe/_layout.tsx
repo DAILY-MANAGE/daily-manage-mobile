08c69862e43b3c33ce17ac63e5ca2e3a25ec3cff
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-10-15 22:30:17 -0300

    new: team layout

--- src/app/equipe/_layout.tsx
+++ src/app/equipe/_layout.tsx
@@ -1,9 +1,36 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
+import React from "react";
+import { Pressable } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function EquipeLayout() {
- return(
-  <Stack>
-   <Stack.Screen name="index" options={{ headerShown: false }} />
-  </Stack>
- );
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "Equipe",
+          headerTitleStyle: {
+            fontWeight: "900",
+          },
+          headerRight: () => (
+            <Link href='/notifications' asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name='bell'
+                    size={20}
+                    style={{
+                      marginRight: 15,
+                      opacity: pressed ? 0.5 : 1,
+                    }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+    </Stack>
+  );
 }
