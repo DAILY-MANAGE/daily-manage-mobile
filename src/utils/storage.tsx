d940cd96b9471f85d37405993857363c9f53c943
arthurm9 <arthurmarquesoliveira001@gmail.com>
2023-10-17 14:04:49 -0300

    deleted

--- src/utils/storage.tsx
+++
@@ -1,9 +0,0 @@
-import Storage from 'react-native-storage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const storage = new Storage({
- storageBackend: AsyncStorage,
- defaultExpires: (1000 * 3600 * 24) / 2,
- sync: {
- }
-});
