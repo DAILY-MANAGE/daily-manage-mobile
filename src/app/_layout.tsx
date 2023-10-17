9361e5f760bf583cf3bd99c18032123c1043f16c
arthurm9 <arthurmarquesoliveira001@gmail.com>
2023-10-17 14:02:14 -0300

    update: screen provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from "expo-router";
+import { setIsLoggedIn } from "./(auth)";
+import { View } from "react-native";
 
 export default function AppLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="equipe" options={{ headerShown: false }} />
+      <Stack.Screen name="provider" options={{ headerShown: false }} />
     </Stack>
   );
 }
