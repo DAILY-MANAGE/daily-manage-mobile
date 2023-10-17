315a6bd3160e0cab90df4013aee42566a14d42fc
arthurm9 <arthurmarquesoliveira001@gmail.com>
2023-10-17 14:03:46 -0300

    update: fix get and post

--- src/app/equipe/index.tsx
+++ src/app/equipe/index.tsx
@@ -1,42 +1,43 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import OverlayComponent from "./components/Overlay";
 import { useState } from "react";
 import axios from "axios";
 import { baseURL } from "../../utils/baseURL";
 import CardEquipes from "./components/Equipes";
-import { storage } from "../../utils/storage";
-
-export const getToken = async () => {
-  storage.getAllDataForKey('token').then(token => {
-    return token;
-  });
-}
+import { getToken } from "../(auth)";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
 });
 
-export default function Equipes() {
+export interface DadosEquipe {
+  id?: number
+  nome?: string
+}
+
+export default function Equipes({ id, nome = '' }: DadosEquipe) {
   const [nomeEquipe, setNomeEquipe] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [equipe, setEquipe] = useState<DadosEquipe[]>([])
 
   const criarEquipe = async () => {
     setIsLoading(true);
+    console.log(await getToken())
     try {
-      const response = await axiosInstance.post(`${baseURL}/equipe/create`, {
+      const response = await axiosInstance.post(`${baseURL}/equipe/criar`, {
         nome: nomeEquipe,
-        token: await getToken(),
       }, {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${await getToken()}`
         },
+        data: {}
       }
       );
-
       if (response.status === 201) {
         console.log(`Equipe criada: ${JSON.stringify(response.data)}`);
         setIsLoading(false);
+        setEquipe(response.data)
       } else {
         throw new Error(
           `Erro ao criar a equipe: ${JSON.stringify(response.data)}`
@@ -49,31 +50,37 @@ export default function Equipes() {
   };
 
   return (
-    <View style={styles.container}>
-      <OverlayComponent
-        value={nomeEquipe}
-        setValue={setNomeEquipe}
-        onPress={criarEquipe}
-        editable={!isLoading}
-      />
-      <View style={styles.cards}>
-        <CardEquipes />
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <OverlayComponent
+          value={nomeEquipe}
+          setValue={setNomeEquipe}
+          onPress={criarEquipe}
+          editable={!isLoading}
+        />
+        <View style={styles.equipes}>
+          <CardEquipes />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  cards: {
-    gap: 16,
+  scrollView: {
     width: "100%",
-    height: "auto",
+    margin: 0, 
+    padding: 0,
   },
   container: {
+    gap: 8,
     height: "100%",
     width: "100%",
     padding: 16,
     flexDirection: "column",
-    gap: 16,
+    margin: 0,
   },
+  equipes: {
+    gap: 8
+  }
 });
