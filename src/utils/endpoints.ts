9e20686c0861215625cf0e73713c41613a7b1cdb
arthurm9 <arthurmarquesoliveira001@gmail.com>
2023-10-20 12:27:20 -0300

    new: endpoints file

---
+++ src/utils/endpoints.ts
@@ -0,0 +1,13 @@
+// API ------------------------------------------------------------
+
+export const baseURL = "http://10.68.20.120:8080"
+
+// GET ------------------------------------------------------------
+
+export const getEquipesTodas = "equipe/todas/criadas"
+export const getFormulariosTodos = "equipe/forms/todos"
+
+// POST -----------------------------------------------------------
+
+export const postFormulario = "equipe/forms"
+export const postEquipe = "equipe/criar"
