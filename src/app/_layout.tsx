bd97148fdf3fb94ad53da78bcd96e62c50b4c6d7
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-20 16:11:44 -0300

    update _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,7 @@ export default function AppLayout() {
   return(
     <Stack>
       <Stack.Screen name="(auth)" options={{ headerShown: false }}/>
-      <Stack.Screen name="equipes" options={{ headerShown: false }}/>
+      <Stack.Screen name="equipes/(tabs)" options={{ headerShown: false }}/>
       <Stack.Screen name="notificacoes" options={{ headerShown: false }}/>
       <Stack.Screen name="equipe/(tabs)" options={{ headerShown: false }}/>
       <Stack.Screen name="criarFormulario" options={{ headerShown: false }}/>
