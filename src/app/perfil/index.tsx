a24d388ee9984bcf3dc3f1348315bb3289831e7a
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-20 16:17:22 -0300

    update src/app/perfil/index.tsx

--- src/app/perfil/index.tsx
+++ src/app/perfil/index.tsx
@@ -1,9 +1,39 @@
-import { Text } from 'react-native';
+import { Text } from 'react-native'
+import CustomInput from '../components/Input'
+import { useState } from 'react'
+import { axiosInstance } from '../../utils/useAxios'
+import { getToken } from '../../hooks/token'
+import { BASEURL } from '../../utils/endpoints'
 
 export default function Perfil() {
- return(
+ const [newName, setNewName] = useState('')
+ const [isLoading, setIsLoading] = useState(false)
+
+ const i = axiosInstance
+
+ /* const changeName = async () => {
+
+  setIsLoading(!isLoading)
+
+  const token = await getToken()
+
+  try {
+    const res = i.patch(`${BASEURL}${EDITAR_NOME_USUARIO}`)
+
+    if()
+  } catch {
+
+  }
+
+ } */
+
+ return (
   <>
-   <Text>Perfil</Text>
+   <CustomInput
+    placeholder='Novo nome'
+    value={newName}
+    setValue={setNewName}
+   />
   </>
- );
+ )
 }
