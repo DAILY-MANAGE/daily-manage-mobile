cc0f1234a66771866ced44b9a62bf79caf0c8627
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-20 16:12:19 -0300

    update _layout.tsx

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,30 +1,34 @@
 import { Stack } from "expo-router";
 import { StyleSheet } from "react-native";
+import BackButton from "../components/BackButton";
 
 export default function AuthLayout() {
   return (
     <Stack>
       <Stack.Screen
         name="index"
-        options={{ 
-          headerShown: false 
+        options={{
+          headerShown: false
         }} />
       <Stack.Screen
         name="cadastro"
         options={{
           headerTitle: 'Cadastro',
           headerStyle: styles.headerStyle,
-          headerTitleStyle: styles.headerTitleStyle
-        }} />
+          headerTitleStyle: styles.headerTitleStyle,
+          headerLeft: () => (<BackButton />),
+        }}
+      />
     </Stack>
   );
 }
 
 const styles = StyleSheet.create({
   headerStyle: {
-    backgroundColor: "#FFFFFF"
+    backgroundColor: "#1C1C1C",
   },
   headerTitleStyle: {
     fontWeight: "900",
+    color: "#FFFFFF"
   }
 });
