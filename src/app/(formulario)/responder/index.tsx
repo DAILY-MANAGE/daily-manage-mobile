4d1871d3d1d1f31077c4cb19248760ac6565affd
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-20 16:12:55 -0300

    update src/app/(formulario)/responder/index.tsx

--- src/app/(formulario)/responder/index.tsx
+++ src/app/(formulario)/responder/index.tsx
@@ -1,21 +1,7 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
 import { View, Text } from "react-native";
-import { useEffect } from "react";
 
 export default function ResponderFormulario() {
-  const getPerguntas = async (): Promise<string[] | null> => {
-    try {
-      const pergunta = await AsyncStorage.getItem("perguntas");
-      if (pergunta !== null) {
-        return JSON.parse(pergunta);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    return null;
-  };
-
   return (
     <>
       <View><Text>aaa</Text></View>
