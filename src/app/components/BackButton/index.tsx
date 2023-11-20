2a5cbd22c0edc755c8e5c25da0451c1706e44cb9
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-20 16:13:09 -0300

    feat: create src/app/components/BackButton/index.tsx

---
+++ src/app/components/BackButton/index.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "expo-router";
+import { Pressable } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+export default function BackButton() {
+ const router = useRouter()
+
+ return (
+  <Pressable onPress={() => router.back()}>
+   {({ pressed }) => (
+    <FontAwesome
+     name="arrow-left"
+     size={20}
+     style={{
+      color: "white",
+      marginLeft: 8,
+      marginRight: 16,
+      opacity: pressed ? 0.5 : 1,
+     }}
+    />
+   )}
+  </Pressable>
+ );
+}
