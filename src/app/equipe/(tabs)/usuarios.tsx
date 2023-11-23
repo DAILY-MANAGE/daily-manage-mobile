27076108dcdd679e6a6aad529d26326a4eb8d13b
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-23 02:16:10 -0300

    update usuarios.tsx

--- src/app/equipe/(tabs)/usuarios.tsx
+++ src/app/equipe/(tabs)/usuarios.tsx
@@ -6,35 +6,35 @@ import {
   ToastAndroid,
   RefreshControl,
   ScrollView,
-} from "react-native";
+} from "react-native"
 import {
   ADICONAR_USUARIO_A_EQUIPE,
   BASEURL,
   FILTRAR_USUARIOS,
   FILTRAR_USUARIOS_DA_EQUIPE,
-} from "../../../utils/endpoints";
-import { useEffect, useState } from "react";
-import { getEquipeId } from "../../equipes/(tabs)";
-import { getToken } from "../../../hooks/token";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { axiosInstance } from "../../../utils/useAxios";
-import CustomButton from "../../components/Button";
-import { Overlay } from "@rneui/themed";
-import CustomInput from "../../components/Input";
-import CustomSearchBar from "../../components/SearchBar/index";
-import React from "react";
-import { useRouter } from "expo-router";
+} from "../../../utils/endpoints"
+import { useEffect, useState } from "react"
+import { getEquipeId } from "../../equipes/(tabs)"
+import { getToken } from "../../../hooks/token"
+import FontAwesome from "@expo/vector-icons/FontAwesome"
+import { axiosInstance } from "../../../utils/useAxios"
+import CustomButton from "../../components/Button"
+import { CheckBox, Overlay } from "@rneui/themed"
+import CustomInput from "../../components/Input"
+import CustomSearchBar from "../../components/SearchBar/index"
+import React from "react"
+import { useRouter } from "expo-router"
 
 export interface DadosUsuario {
-  id?: number;
-  usuario?: string;
-  nome?: string;
+  id?: number
+  usuario?: string
+  nome?: string
 }
 
 interface PermPreset {
-  id: number;
-  name: string;
-  value: string;
+  id: number
+  name: string
+  value: string
 }
 
 const permPreset: PermPreset[] = [
@@ -68,46 +68,50 @@ const permPreset: PermPreset[] = [
     name: "Ver respostas",
     value: "VER_FORMULARIO_RESPONDIDO",
   },
-];
+]
 
 export default function Users() {
-  const [newUser, setNewUser] = useState<DadosUsuario[]>([]);
-  const [teamUsers, setTeamUsers] = useState<DadosUsuario[]>([]);
-  const [allUsers, setAllUsers] = useState<DadosUsuario[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [visible, setVisible] = useState(false);
-  const [perm, setPerm] = useState("");
-  const [search, setSearch] = useState("");
-  const [usrId, setUsrId] = useState(0);
-  const [usrName, setUsrName] = useState("");
-  const [refreshing, setRefreshing] = useState(false);
+  const [newUser, setNewUser] = useState<DadosUsuario[]>([])
+  const [teamUsers, setTeamUsers] = useState<DadosUsuario[]>([])
+  const [allUsers, setAllUsers] = useState<DadosUsuario[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [visible, setVisible] = useState(false)
+  const [perm, setPerm] = useState("")
+  const [search, setSearch] = useState("")
+  const [usrId, setUsrId] = useState(0)
+  const [usrName, setUsrName] = useState("")
+  const [refreshing, setRefreshing] = useState(false)
+  const [selectedUserId, setSelectedUserId] = useState<number | null>(null)
+  const [selectedUser, setSelectedUser] = useState<string | null>(null)
+  const [selectedPermissions, setSelectedPermissions] = useState<string[]>(["VISUALIZAR_FORMULARIO"])
 
-  const router = useRouter();
+  const router = useRouter()
 
   const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
+    setRefreshing(true)
     setTimeout(() => {
-      setRefreshing(false);
-      router.replace("equipe");
-    }, 500);
-  }, []);
+      setRefreshing(false)
+      router.replace("equipe")
+    }, 500)
+  }, [])
 
   const handleSearch = (text: string) => {
-    setSearch(text);
-  };
+    setSearch(text)
+  }
 
   const toggleOverlay = () => {
-    setVisible(!visible);
-  };
+    setVisible(!visible)
+    resetState()
+  }
 
-  const i = axiosInstance;
+  const i = axiosInstance
 
   async function getTeamUsers() {
-    setIsLoading(true);
+    setIsLoading(true)
     try {
-      const token = await getToken();
+      const token = await getToken()
 
-      const equipeid = await getEquipeId();
+      const equipeid = await getEquipeId()
 
       const res = await i.get(`${BASEURL}${FILTRAR_USUARIOS_DA_EQUIPE}`, {
         headers: {
@@ -116,31 +120,31 @@ export default function Users() {
           Equipe: equipeid as number,
         },
         data: {},
-      });
+      })
 
       if (res.status === 200) {
-        setTeamUsers(res.data.content);
-        setIsLoading(false);
+        setTeamUsers(res.data.content)
+        setIsLoading(false)
       } else {
-        throw new Error(`${JSON.stringify(res.data)}`);
+        throw new Error(`${JSON.stringify(res.data)}`)
       }
     } catch (err) {
-      console.log(err);
-      setIsLoading(false);
+      console.log(err)
+      setIsLoading(false)
     }
   }
 
   useEffect(() => {
-    getTeamUsers();
-  }, []);
+    getTeamUsers()
+  }, [])
 
   async function getAllUsers() {
-    setIsLoading(true);
-    console.log(allUsers);
+    setIsLoading(true)
+    console.log(allUsers)
     try {
-      const token = await getToken();
+      const token = await getToken()
 
-      const equipeid = await getEquipeId();
+      const equipeid = await getEquipeId()
 
       const res = await i.get(`${BASEURL}${FILTRAR_USUARIOS}?nome=${search}`, {
         headers: {
@@ -149,38 +153,36 @@ export default function Users() {
           Equipe: equipeid as number,
         },
         data: {},
-      });
+      })
 
       if (res.status === 200) {
-        setAllUsers(res.data.content);
-        setIsLoading(false);
+        setAllUsers(res.data.content)
+        setIsLoading(false)
       } else {
-        throw new Error(`${JSON.stringify(res.data)}`);
+        throw new Error(`${JSON.stringify(res.data)}`)
       }
     } catch (err) {
-      console.log(err);
-      setIsLoading(false);
+      console.log(err)
+      setIsLoading(false)
     }
   }
 
   useEffect(() => {
-    getAllUsers();
-  }, [setVisible]);
+    getAllUsers()
+  }, [search])
 
   const addUser = async () => {
-    setIsLoading(!isLoading);
+    setIsLoading(!isLoading)
 
-    const token = await getToken();
+    const token = await getToken()
 
-    const equipeid = await getEquipeId();
-
-    const usuarioid = usrId;
+    const equipeid = await getEquipeId()
 
     try {
       const res = await i.post(
-        `${BASEURL}${ADICONAR_USUARIO_A_EQUIPE}/${usuarioid}`,
+        `${BASEURL}${ADICONAR_USUARIO_A_EQUIPE}/${usrId}`,
         {
-          permissoes: [perm],
+          permissoes: [...selectedPermissions],
         },
         {
           headers: {
@@ -190,27 +192,37 @@ export default function Users() {
           },
           data: {},
         }
-      );
+      )
 
       if (res.status === 200) {
-        setNewUser(res.data.content);
+        setNewUser(res.data.content)
         ToastAndroid.show(
           `Usuário ${usrName} convidado para a equipe`,
           ToastAndroid.SHORT
-        );
-        setIsLoading(!isLoading);
+        )
+        resetState()
+        setIsLoading(!isLoading)
       } else {
-        throw new Error(`${JSON.stringify(res.data)}`);
+        throw new Error(`${JSON.stringify(res.data)}`)
       }
     } catch (err) {
-      console.log(err);
-      setIsLoading(!isLoading);
+      console.log(err)
+      setIsLoading(!isLoading)
     }
-  };
+  }
 
   const filteredUsers = allUsers.filter((usr) =>
     search ? usr.nome.toLowerCase().includes(search.toLowerCase()) : true
-  );
+  )
+
+  const resetState = () => {
+    setUsrId(0)
+    setUsrName("")
+    setSearch("")
+    setSelectedUserId(null)
+    setSelectedUser('')
+    setSelectedPermissions(["VISUALIZAR_FORMULARIO"])
+  }
 
   return (
     <>
@@ -251,40 +263,92 @@ export default function Users() {
           value={search}
           onChangeText={handleSearch}
         />
-        {search.length === 0 ? (
-          <Text>Nenhum usuário foi encontrado...</Text>
-        ) : (
-          search &&
-          filteredUsers.map((usr: DadosUsuario) => (
-            <Pressable
-              key={usr.id}
-              onPress={() => {
-                setUsrId(usr.id), setUsrName(usr.nome), console.log(usr.id);
-              }}
-            >
-              <Text>{usr.nome}</Text>
-              <Text>ID: {usr.id}</Text>
-            </Pressable>
-          ))
-        )}
+        <ScrollView style={styles.searchResults}>
+          {search.length === 0 ? (
+            <Text>Nenhum usuário foi encontrado...</Text>
+          ) : (
+            search &&
+            filteredUsers.map((usr: DadosUsuario) => (
+              <Pressable
+                key={usr.id}
+                onPress={() => {
+                  setUsrId(usr.id),
+                    setUsrName(usr.nome),
+                    console.log(usr.id),
+                    setSelectedUserId(usr.id),
+                    setSearch("")
+                  setSelectedUser(usr.usuario)
+                }}
+                style={[
+                  styles.userItem,
+                  usr.id === selectedUserId && styles.selectedUserItem,
+                ]}
+              >
+                <Text>{usr.nome}</Text>
+                <Text>ID: {usr.id}</Text>
+              </Pressable>
+            ))
+          )}
+        </ScrollView>
+        {selectedUser ? (<Text>{selectedUser}</Text>) : ('')}
         {permPreset &&
           permPreset.map((perm: PermPreset) => (
-            <Pressable
+            <View
+              style={styles.permissionItem}
               key={perm.id}
-              onPress={() => {
-                setPerm(perm.value), console.log(perm.value);
-              }}
             >
+              <CheckBox
+                checked={selectedPermissions.includes(perm.value)}
+                onPress={() => {
+                  //                   if (perm.value === "VISUALIZAR_FORMULARIO") return deixa visualizar como default
+                  setSelectedPermissions((prevPermissions) => {
+                    if (prevPermissions.includes(perm.value)) {
+                      return prevPermissions.filter((p) => p !== perm.value)
+                    }
+                    return [...prevPermissions, perm.value]
+                  })
+                }}
+              />
               <Text>{perm.name}</Text>
-            </Pressable>
+            </View>
           ))}
         <CustomButton onPress={addUser} title="+ Adicionar" />
       </Overlay>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
+  searchResults: {
+    position: 'absolute',
+    bottom: 0,
+    left: '50%',
+    transform: [{ translateX: -50 }],
+    width: '90%',
+    maxHeight: 150,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#c5c5c5",
+    marginTop: 8,
+  },
+  permissionItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 4,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#c5c5c5",
+  },
+  userItem: {
+    padding: 8,
+    marginVertical: 4,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#c5c5c5",
+  },
+  selectedUserItem: {
+    backgroundColor: "#e0e0e0",
+  },
   overlayStyle: {
     borderRadius: 16,
     padding: 16,
@@ -336,4 +400,4 @@ const styles = StyleSheet.create({
     fontWeight: "normal",
     color: "#666564",
   },
-});
+})
