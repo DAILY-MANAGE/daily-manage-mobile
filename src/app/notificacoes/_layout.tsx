36ad40f82ca787f77b811195f065edd215c540d9
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-24 05:08:52 -0300

    update _layout.tsx

--- src/app/notificacoes/_layout.tsx
+++ src/app/notificacoes/_layout.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "expo-router";
-import React from "react";
-import { StyleSheet } from 'react-native';
+import { Stack } from "expo-router"
+import React from "react"
+import { StyleSheet } from 'react-native'
+import BackButton from '../components/BackButton/index'
 
 export default function NotificacoesLayout() {
   return (
@@ -9,15 +10,21 @@ export default function NotificacoesLayout() {
         name="index"
         options={{
           headerTitle: "Notificações",
+          headerStyle: styles.headerStyle,
           headerTitleStyle: styles.headerTitleStyle,
+          headerLeft: () => (<BackButton />)
         }}
       />
     </Stack>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: "#1C1C1C"
+  },
   headerTitleStyle: {
-    fontWeight: "900"
-  }
-});
+    fontWeight: "500",
+    color: "white",
+  },
+})
