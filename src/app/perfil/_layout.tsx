b140d474cbf32caa058c39804d9a64927814ce1f
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-24 05:09:00 -0300

    update _layout.tsx

--- src/app/perfil/_layout.tsx
+++ src/app/perfil/_layout.tsx
@@ -1,26 +1,39 @@
-import { Stack } from "expo-router";
-import { StyleSheet } from 'react-native';
-import { DadosUsuario } from "../equipe/(tabs)/usuarios";
+import { Stack, useRouter } from "expo-router"
+import { StyleSheet } from 'react-native'
+import BackButton from "../components/BackButton"
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 export default function PerfilLayout() {
- return (
-  <Stack>
-   <Stack.Screen
-    name="index"
-    options={{
-     headerTitle: 'Perfil',
-     headerStyle: styles.headerStyle,
-     headerTitleStyle: styles.headerTitleStyle
-    }} />
-  </Stack>
- );
+  const router = useRouter()
+
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: 'Perfil',
+          headerStyle: styles.headerStyle,
+          headerTitleStyle: styles.headerTitleStyle,
+          headerLeft: () => (<BackButton />),
+          headerRight: () => (
+            <FontAwesome
+              name="sign-out"
+              color="red"
+              size={24}
+              onPress={() => router.replace("(auth)")}
+            />
+          )
+        }} />
+    </Stack>
+  )
 }
 
 const styles = StyleSheet.create({
- headerStyle: {
-  backgroundColor: "#FFFFFF"
- },
- headerTitleStyle: {
-  fontWeight: "900",
- }
-});
+  headerStyle: {
+    backgroundColor: "#1C1C1C"
+  },
+  headerTitleStyle: {
+    fontWeight: "900",
+    color: "#FFFFFF"
+  }
+})
