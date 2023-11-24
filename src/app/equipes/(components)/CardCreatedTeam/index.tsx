e811326b8487923897aa25aece4318ee99c39dbc
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2023-11-23 21:09:05 -0300

    update src/app/equipes/(components)/CardCreatedTeam/index.tsx

--- src/app/equipes/(components)/CardCreatedTeam/index.tsx
+++ src/app/equipes/(components)/CardCreatedTeam/index.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { BASEURL, VER_EQUIPES_CRIADAS } from "../../../../utils/endpoints";
-import { DadosEquipe } from "../../../../interfaces/DadosEquipe";
-import { getToken } from "../../../../hooks/token";
-import { IdStorage } from "../../../../hooks/useId";
-import { useRouter } from "expo-router";
-import { axiosInstance } from "../../../../utils/useAxios";
+import React, { useEffect, useState } from "react"
+import { Pressable, StyleSheet, Text, View } from "react-native"
+import { BASEURL, VER_EQUIPES_CRIADAS } from "../../../../utils/endpoints"
+import { DadosEquipe } from "../../../../interfaces/DadosEquipe"
+import { getToken } from "../../../../hooks/token"
+import { IdStorage } from "../../../../hooks/useId"
+import { useRouter } from "expo-router"
+import { axiosInstance } from "../../../../utils/useAxios"
 
 export function CardCreatedTeam({ search }: { search: string }) {
-  const [data, setData] = useState<DadosEquipe[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<DadosEquipe[]>([])
+  const [isLoading, setIsLoading] = useState(false)
 
-  const router = useRouter();
+  const router = useRouter()
 
-  const i = axiosInstance;
+  const i = axiosInstance
 
   async function getCreatedTeams() {
-    const token = await getToken();
+    const token = await getToken()
 
-    setIsLoading(true);
+    setIsLoading(true)
 
     try {
       const res = await i.get(`${BASEURL}${VER_EQUIPES_CRIADAS}`, {
@@ -27,63 +27,57 @@ export function CardCreatedTeam({ search }: { search: string }) {
           Authorization: `Bearer ${token}`,
         },
         data: {},
-      });
-
-      setTimeout(() => {
-        setIsLoading(false);
-      }, 2000)
+      })
       if (res.status === 200) {
-        setData(res.data.content);
+        const updatedData = res.data.content
+        setData(updatedData)
+        setIsLoading(false)
+        console.log(updatedData)
       } else {
-        return Promise.reject()
+        throw new Error(`${JSON.stringify(res.data)}`)
       }
     } catch (err) {
-      setIsLoading(false);
-      return Promise.reject()
+      console.log(err)
+      setIsLoading(false)
     }
-    return Promise.resolve()
   }
-
-  useEffect(() => {
-    const abortController = new AbortController();
-    const fetchData = async () => await getCreatedTeams();
-    fetchData();
-
-    return () => {
-      abortController.abort();
-    };
-  }, [BASEURL, VER_EQUIPES_CRIADAS, i]);
 
   const filteredTeams = data.filter((team) =>
     search ? team.nome.toLowerCase().includes(search.toLowerCase()) : true
-  );
+  )
+
+  useEffect(() => {
+    getCreatedTeams()
+  }, []) // precisa arrumar isso
 
   return (
     <>
-      {isLoading && (<Text>Buscando equipes...</Text>)}
       {
-      filteredTeams.length === 0 ? (
-        <Text>Nenhuma equipe foi encontrada...</Text>
-      ) : (
-        filteredTeams &&
-        filteredTeams.map((team: DadosEquipe) => (
-          <Pressable
-            key={team.id}
-            style={styles.equipeContainer}
-            onPress={() => {
-              IdStorage.setId(team.id as any);
-              router.push('/equipe/(tabs)');
-            }}
-          >
-            <View key={team.id} style={styles.container}>
-              <Text style={styles.title}>{team.nome}</Text>
-              <Text style={styles.subitle}>Identificação: {team.id}</Text>
-            </View>
-          </Pressable>
-        ))
-      )}
+        filteredTeams && filteredTeams.length === 0 ? (
+          <Text>Nenhuma equipe foi encontrada...</Text>
+        ) :
+          filteredTeams &&
+          filteredTeams.map((team: DadosEquipe) => (
+            <Pressable
+              key={team.id}
+              style={styles.equipeContainer}
+              onPress={() => {
+                IdStorage.setId(team.id as any)
+                router.push({
+                  pathname: `/equipe/(tabs)/${team.id as any}`,
+                  params: { equipeid: team.id as any}
+                })
+              }}
+            >
+              <View key={team.id} style={styles.container}>
+                <Text style={styles.title}>{team.nome}</Text>
+                <Text style={styles.subitle}>Identificação: {team.id}</Text>
+              </View>
+            </Pressable>
+          ))
+      }
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -114,4 +108,4 @@ const styles = StyleSheet.create({
     fontWeight: "normal",
     color: "#666564",
   },
-});
+})
